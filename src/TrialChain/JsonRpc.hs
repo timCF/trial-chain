a0fc0af2dd8ffe2caee07bcc18130519a7b419c5
timcf <timcf@89-235-229-125.saturn.infonet.ee>
2019-09-22 19:19:30 +0300

    refactor TrialChain.JsonRpc

--- src/TrialChain/JsonRpc.hs
+++ src/TrialChain/JsonRpc.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module TrialChain.JsonRpc (apply) where
-import           Control.Monad.Except   (ExceptT, throwError)
+import           Control.Monad.Except   (throwError)
 import           Control.Monad.Trans    (liftIO)
 import           Data.ByteString.Lazy
 import           Network.JsonRpc.Server
@@ -23,21 +23,14 @@ add :: NodeApi -> Method IO
 add nodeApi = toMethod "add" f (Required "x" :+: Required "y" :+: ())
   where
     f :: Integer -> Integer -> RpcResult IO Integer
-    f x y = do
-      res <- liftIO $ broadcastTrx nodeApi $ x + y
-      case res of
-        Just it -> return it
-        Nothing -> internalError
+    f x y = liftIO (broadcastTrx nodeApi $ x + y) >>= unwrapRes
 
 echo :: NodeApi -> Method IO
 echo nodeApi = toMethod "echo" f (Required "x" :+: ())
   where
     f :: String -> RpcResult IO String
-    f x = do
-      res <- liftIO $ echoMsg nodeApi x
-      case res of
-        Just it -> return it
-        Nothing -> internalError
+    f x = liftIO (echoMsg nodeApi x) >>= unwrapRes
 
-internalError :: ExceptT RpcError IO a
-internalError = throwError $ rpcError (-32603) "internal error"
+unwrapRes :: Maybe a -> RpcResult IO a
+unwrapRes (Just it) = return it
+unwrapRes Nothing   = throwError $ rpcError (-32603) "internal error"
